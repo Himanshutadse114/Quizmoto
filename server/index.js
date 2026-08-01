b24fd6dde76b87d77ab9042f905e9949c5af0a81
samruddhi bhiwgade <samruddhi96583.com>
2026-08-02 01:59:06 +0530

    Fix payload too large error for base64 images by increasing express limit

--- server/index.js
+++ server/index.js
@@ -54,8 +54,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // DB Connection & Start Server
