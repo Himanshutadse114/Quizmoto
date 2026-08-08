57c1e0a767951b0bafee0fd19cdd66c127fc28f7
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-08-08 16:36:09 +0530

    fix(scorm): use theme-aware animated vector generator

--- server/services/scorm/ScormVisualAssetService.js
+++ server/services/scorm/ScormVisualAssetService.js
@@ -30,7 +30,7 @@ function pythonCandidates() {
 }
 
 async function runVisualGenerator(inputPath, outputDir) {
-    const script = path.join(__dirname, '../../utils/generate_scorm_visuals_v2.py');
+    const script = path.join(__dirname, '../../utils/generate_scorm_visuals_v3.py');
     if (!fs.existsSync(script)) {
         const err = new Error('SCORM visual generator script is missing');
         err.code = 'SCORM_VISUAL_SCRIPT_MISSING';
