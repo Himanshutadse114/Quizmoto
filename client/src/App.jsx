f03e360e16a9b1644efa4ea5cbef0d9c4c424f86
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-08-08 16:17:14 +0530

    chore(scorm): remove Voice Lab route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,6 @@ import ScormLearnLanding from './pages/Scorm/LearnLanding';
 import ScormPlayerShell from './pages/Scorm/PlayerShell';
 import ScormAuthor from './pages/Scorm/Author';
 import ScormReports from './pages/Scorm/Reports';
-import ScormVoicePreview from './pages/Scorm/VoicePreview';
 
 function App() {
   return (
@@ -64,7 +63,6 @@ function App() {
               <Route path="/scorm" element={<ScormHome />} />
               <Route path="/scorm/library" element={<ScormLibrary />} />
               <Route path="/scorm/author" element={<ScormAuthor />} />
-              <Route path="/scorm/voice-preview" element={<ScormVoicePreview />} />
               <Route path="/scorm/reports" element={<ScormReports />} />
               <Route path="/scorm/courses/:id" element={<ScormCourseDetail />} />
               <Route path="/scorm/learn/:inviteCode" element={<ScormLearnLanding />} />
