d9ab87b121d01c61a21f11c331350b0a040b6269
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-08-15 20:09:41 +0530

    Mount SCORM access management routes

--- server/routes/scorm/index.js
+++ server/routes/scorm/index.js
@@ -20,6 +20,7 @@ router.use('/content', require('./content'));
 router.use('/play', require('./play'));
 router.use('/xapi', require('./xapi'));
 router.use('/author', require('./author'));
+router.use('/access', require('./access'));
 
 router.get('/features', (req, res) => {
     res.json({
