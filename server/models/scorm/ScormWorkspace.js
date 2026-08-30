2ff2a5c23e1e127f856e3d3b0ad70476ee8c04d7
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-08-30 22:48:08 +0530

    Add SCORM workspace model

---
+++ server/models/scorm/ScormWorkspace.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../config/database');
+
+const ScormWorkspace = sequelize.define('ScormWorkspace', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    ownerUserId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+    },
+    name: {
+        type: DataTypes.STRING(160),
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        defaultValue: 'active'
+    }
+}, {
+    tableName: 'scorm_workspaces',
+    indexes: [
+        { unique: true, fields: ['ownerUserId'] },
+        { fields: ['status'] }
+    ]
+});
+
+module.exports = ScormWorkspace;
