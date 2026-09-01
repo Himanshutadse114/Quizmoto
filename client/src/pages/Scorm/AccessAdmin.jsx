8b3f72c5139a2684d12f9360bb025ebef3ccb409
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-02 00:08:21 +0530

    Show SMTP diagnostics in Super Admin

--- client/src/pages/Scorm/AccessAdmin.jsx
+++ client/src/pages/Scorm/AccessAdmin.jsx
@@ -1,1 +1,12 @@
-export { default } from './TenantAdmin';
+import React from 'react';
+import TenantAdmin from './TenantAdmin';
+import SuperAdminMailPanel from './SuperAdminMailPanel';
+
+export default function AccessAdmin() {
+  return (
+    <>
+      <SuperAdminMailPanel />
+      <TenantAdmin />
+    </>
+  );
+}
