1ea042dc4b7473912bf7c7d9cd4bf6952fefdc67
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-02 18:07:08 +0530

    Enable shared LMSGEN navigation cache

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './services/scormApiCache.js'
 import App from './App.jsx'
 import CampaignManagementDock from './components/CampaignManagementDock.jsx'
 import './campaignDividerFix.css'
