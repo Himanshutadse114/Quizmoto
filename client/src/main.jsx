d72be5dc9a97b560411b864631c2e4daaef7ae7b
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-02 16:38:50 +0530

    Mount active campaign management controls

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import CampaignManagementDock from './components/CampaignManagementDock.jsx'
 import './campaignDividerFix.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <CampaignManagementDock />
   </StrictMode>,
 )
