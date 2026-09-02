66fb618fa2893483796472a2e0227190dfa98826
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-02 20:08:53 +0530

    Start LMSGEN data warmup with the app shell

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import './services/scormApiCache.js'
 import App from './App.jsx'
 import CampaignManagementDock from './components/CampaignManagementDock.jsx'
+import PlatformDataBootstrap from './components/PlatformDataBootstrap.jsx'
 import './campaignDividerFix.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PlatformDataBootstrap />
     <CampaignManagementDock />
   </StrictMode>,
 )
