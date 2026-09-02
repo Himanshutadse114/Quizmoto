0423770b304680e16c2268ab4e6495a65fb13524
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-02 21:05:20 +0530

    Remove legacy inline campaign management dock

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,12 @@ import './index.css'
 import './services/scormApiCache.js'
 import './services/campaignAdminReadRedirect.js'
 import App from './App.jsx'
-import CampaignManagementDock from './components/CampaignManagementDock.jsx'
 import PlatformDataBootstrap from './components/PlatformDataBootstrap.jsx'
-import './campaignDividerFix.css'
 import './lmsgenLightFinal.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <PlatformDataBootstrap />
-    <CampaignManagementDock />
   </StrictMode>,
 )
