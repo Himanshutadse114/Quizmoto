aee02fbbe340af357aa882d5dcd28036a1c385e9
Himanshutadse114 <83593850+Himanshutadse114@users.noreply.github.com>
2026-09-09 16:35:47 +0530

    Clean flipbook route imports

--- server/routes/flipbooks.js
+++ server/routes/flipbooks.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { Op } = require('sequelize');
 const router = express.Router();
 const User = require('../models/User');
 const Flipbook = require('../models/Flipbook');
